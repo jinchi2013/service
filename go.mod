e03d80580e13f7d9c67e5e50bebc64e762b2e75c
CHI JIN <jinchi2013us@gmail.com>
2022-04-09 11:55:58 -0400

    simple service

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/jinchi2013/service
 
 go 1.18
-
-require github.com/ardanlabs/conf v1.5.0
--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"github.com/ardanlabs/conf"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
+var build = "develop"
+
 func main() {
-	conf.New()
+	log.Println("staring service", build)
+	defer log.Println("service ended")
+
+	shutDown := make(chan os.Signal, 1)
+	signal.Notify(shutDown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutDown
+
+	log.Println("stopping service")
 }
