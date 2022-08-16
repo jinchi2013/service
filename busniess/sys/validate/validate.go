13a32fcfc108d70afa799c81d3a1b8d93485fecc
CHI JIN <jinchi2013us@gmail.com>
2022-08-16 18:25:37 -0400

    add go-playground/validator/v10

--- busniess/sys/validate/validate.go
+++ busniess/sys/validate/validate.go
@@ -1,1 +1,9 @@
+// Package validate contains the support for validating models
 package validate
+
+import (
+	"github.com/go-playground/validator/v10"
+)
+
+// validate holds the settings and caches for validating request struct values
+var validate *validator.Validate
